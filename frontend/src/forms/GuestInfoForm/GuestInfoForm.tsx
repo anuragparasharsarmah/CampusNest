989d25b9386f82641c6d012790b8754c06a41064
anuragpsarmah <anurag.parashar@mca.christuniversity.in>
2024-03-05 14:36:13 +0530

    rounded

--- frontend/src/forms/GuestInfoForm/GuestInfoForm.tsx
+++ frontend/src/forms/GuestInfoForm/GuestInfoForm.tsx
@@ -142,7 +142,7 @@ const GuestInfoForm = ({ hotelId, pricePerNight }: Props) => {
             )}
           </div> */}
           {isLoggedIn ? (
-            <button className="bg-blue-600 text-white h-full p-2 font-bold hover:bg-blue-500 text-xl">
+            <button className="bg-blue-600 text-white h-full p-2 font-bold hover:bg-blue-500 text-xl rounded-lg boxs">
               Book Now
             </button>
           ) : (
